1c463721eeaaa16c1683cf16680b209313787ab1
Abinavsudharshan <abinavsudharshan4699@gmail.com>
2019-12-25 11:11:58 +0530

    Updates

---
+++ precache-manifest.e2f5997315b4e90f2f2fde38436ee542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f74dd87951a27fa9d2da0d2afc052e",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "d5aad951685a8f113373",
+    "url": "/helloworld/static/css/main.184931c8.chunk.css"
+  },
+  {
+    "revision": "be2a139f0af26d3473de",
+    "url": "/helloworld/static/js/2.731426fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/helloworld/static/js/2.731426fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5aad951685a8f113373",
+    "url": "/helloworld/static/js/main.fd4f7ba1.chunk.js"
+  },
+  {
+    "revision": "6e36fee57452d994484c",
+    "url": "/helloworld/static/js/runtime-main.51032907.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloworld/static/media/logo.5d5d9eef.svg"
+  }
+]);
